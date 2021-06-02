54deb830eb7344c54ba0faeb58c00097fe9717ab
Jdfg25 <janek2017@web.de>
2021-06-02 11:04:25 +0200

    Update Evaluate.py

--- Evaluate.py
+++ Evaluate.py
@@ -27,9 +27,14 @@ def main():
 
     print(classifier('berlin ist die [MASK] von deutschland'))
     print(classifier('mein [MASK] ist peter'))
-    print(classifier('ich habe diesen [MASK] geschaut und er war großartig'))
+    print(classifier('ich habe diesen [MASK] geschaut und er war fantastisch'))
     print(classifier('dieser film stammt aus dem [MASK] 1980'))
     print(classifier('heute scheint die [MASK]'))
+    print(classifier('die nähe zum meer sorgt für eine [MASK] luftfeuchtigkeit'))
+    print(classifier('angela merkel ist eine deutsche [MASK]'))
+    print(classifier('Christentum, [MASK] und Judentum sind Religionen'))
+    print(classifier('ein automobil ist ein von einem [MASK] angetriebenes straßenfahrzeug'))
+    print(classifier('das alphabet besteht aus [MASK]'))
 
 
 if __name__ == '__main__':

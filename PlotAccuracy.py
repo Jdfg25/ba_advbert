258549b4af9554978469d1f103c3f36b8123be0e
Jdfg25 <janek2017@web.de>
2021-06-16 13:17:59 +0200

    Update PlotAccuracy.py

--- PlotAccuracy.py
+++ PlotAccuracy.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = '/tmp/accuracy/accuracy.txt'
+path = r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_40_custom\accuracy_files\accuracy_train.txt'
 
 with open(path, 'r') as f:
     lines = f.readlines()
-    x = [float(line.split('accuracy ')[1].split('\n')[0]) for line in lines]
+    x = [float(line.split('accuracy ')[1].split('\n')[0]) for i, line in enumerate(lines) if i % 2]
 
-values = pd.Series(x, dtype=object)
+values = pd.Series(data=x, dtype=object)
+print(values)
 
 f = plt.figure()
 f.set_figwidth(15)
 
 values.plot()
 plt.savefig(
-    fname='/tmp/accuracy/acc_plot'
+    fname=r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_40_custom\accuracy_files\accuracy_train_plot'
 )
 plt.show()

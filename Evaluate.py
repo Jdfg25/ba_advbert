fd5f0055c0032c1986b474de4077740106eee987
Jdfg25 <janek2017@web.de>
2021-07-05 13:23:21 +0200

    Update Evaluate.py

--- Evaluate.py
+++ Evaluate.py
@@ -4,11 +4,13 @@ from transformers import pipeline, AutoTokenizer, AutoModelForMaskedLM
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
         "--typos",
         type=bool,
         default=False,
     )
+
     parser.add_argument(
         "--model_name_or_path",
         type=str,
@@ -22,53 +24,27 @@ def parse_args():
 def main():
     args = parse_args()
 
-    model = AutoModelForMaskedLM.from_pretrained(
-        r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_20_custom\model'
-    )
+    model = AutoModelForMaskedLM.from_pretrained(args.model_name_or_path)
     tokenizer = AutoTokenizer.from_pretrained('dbmdz/bert-base-german-uncased')
 
     classifier = pipeline('fill-mask', model=model, tokenizer=tokenizer)
 
     if not args.typos:
         print(classifier('berlin ist die [MASK] von deutschland'))
-        print('\n')
         print(classifier('mein [MASK] ist peter'))
-        print('\n')
         print(classifier('ich habe diesen [MASK] geschaut und er war fantastisch'))
-        print('\n')
         print(classifier('dieser film stammt aus dem [MASK] 1980'))
-        print('\n')
-        print(classifier('heute scheint die [MASK]'))
-        print('\n')
         print(classifier('die nähe zum meer sorgt für eine [MASK] luftfeuchtigkeit'))
-        print('\n')
-        print(classifier('angela merkel ist eine deutsche [MASK]'))
-        print('\n')
-        print(classifier('Christentum, [MASK] und Judentum sind Religionen'))
-        print('\n')
+        print(classifier('christentum, [MASK] und judentum sind religionen'))
         print(classifier('ein automobil ist ein von einem [MASK] angetriebenes straßenfahrzeug'))
-        print('\n')
-        print(classifier('das alphabet besteht aus [MASK]'))
     else:
         print(classifier('berl4n ist die [MASK] von deutschland'))
-        print('\n')
         print(classifier('mein [MASK] istn peter'))
-        print('\n')
         print(classifier('ih habe diesen [MASK] geschaut und er war fantastisch'))
-        print('\n')
         print(classifier('dieser fil mstammt aus dem [MASK] 1980'))
-        print('\n')
-        print(classifier('heute scheit die [MASK]'))
-        print('\n')
         print(classifier('die nähe zum meer sorgt für eine [MASK] luftfeuchtiäkeit'))
-        print('\n')
-        print(classifier('angela merkel ist eine deutsc1he [MASK]'))
-        print('\n')
         print(classifier('Christentum, [MASK] und Judentum sind Religionne'))
-        print('\n')
         print(classifier('ein automobil istein von einem [MASK] angetriebenes straßenfahrzeug'))
-        print('\n')
-        print(classifier('das alphabet becsteht aus [MASK]'))
 
 
 if __name__ == '__main__':

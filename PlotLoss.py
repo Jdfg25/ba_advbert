5f3a71e5244dce129db12298d858ba9726c6dc3e
Jdfg25 <janek2017@web.de>
2021-07-05 13:24:13 +0200

    Update PlotLoss.py

--- PlotLoss.py
+++ PlotLoss.py
@@ -1,19 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_20_custom\loss_files\losses_eval.txt'
 
-with open(path, 'r') as f:
-    lines = f.readlines()
-    x = [float(line.split('loss ')[1].split('\n')[0]) for line in lines]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--path",
+        type=str,
+        default=None,
+        required=True
+    )
 
-values = pd.Series(x, dtype=object)
+    parser.add_argument(
+        "--eval",
+        type=bool,
+        default=False,
+    )
 
-f = plt.figure()
-f.set_figwidth(15)
+    args = parser.parse_args()
+    return args
 
-values.plot()
-plt.savefig(
-    fname=r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_20_custom\loss_files\losses_eval_plot'
-)
-plt.show()
+
+def main():
+    args = parse_args()
+
+    if args.eval:
+        path_part = 'eval'
+    else:
+        path_part = 'train'
+
+    path = args.path + '/losses_' + path_part + '.txt'
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        x = [float(line.split('loss ')[1].split('\n')[0]) for line in lines]
+
+    values = pd.Series(x, dtype=object)
+    print(values)
+
+    f = plt.figure()
+    f.set_figwidth(15)
+
+    values.plot()
+    plt.savefig(fname=args.path + '/losses_' + path_part + '_plot')
+    plt.show()

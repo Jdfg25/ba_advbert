b1bec29651a5c7a6e3c94ab7e939b84f53eefdbd
Jdfg25 <janek2017@web.de>
2021-07-05 13:24:08 +0200

    Update PlotAccuracy.py

--- PlotAccuracy.py
+++ PlotAccuracy.py
@@ -1,20 +1,40 @@
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_40_custom\accuracy_files\accuracy_train.txt'
 
-with open(path, 'r') as f:
-    lines = f.readlines()
-    x = [float(line.split('accuracy ')[1].split('\n')[0]) for i, line in enumerate(lines) if i % 2]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--path",
+        type=str,
+        default=None,
+        required=True
+    )
 
-values = pd.Series(data=x, dtype=object)
-print(values)
+    args = parser.parse_args()
+    return args
 
-f = plt.figure()
-f.set_figwidth(15)
 
-values.plot()
-plt.savefig(
-    fname=r'E:\Uni\9. Trimester (Bachelorarbeit)\Dateien vom Monacum One\notypos_40_custom\accuracy_files\accuracy_train_plot'
-)
-plt.show()
+def main():
+    args = parse_args()
+
+    path = args.path + '/accuracy_eval_per_epoch.txt'
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        x = [float(line.split('accuracy ')[1].split('\n')[0]) for i, line in enumerate(lines) if i % 2]
+
+    values = pd.Series(data=x, dtype=object)
+    print(values)
+
+    f = plt.figure()
+    f.set_figwidth(15)
+
+    values.plot()
+    plt.savefig(fname=args-path + '/accuracy_eval_plot')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
